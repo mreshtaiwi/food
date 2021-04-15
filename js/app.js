f8a1194aed84a4ce7813977fbae24a7185eb3f00
mreshtaiwi <>
2021-04-15 10:20:23 +0000

    adding loops

---
+++ js/app.js
@@ -0,0 +1,146 @@
+// // alert('hello');
+// // console.log('this is from js file');
+// /* this is
+
+//  a comment */
+// var userName = 'ethar'; //string
+// var age = 35; //number
+// var loveCoding = true; //boolean
+
+// var age = '35'; //string
+
+// var titleName = '<h1> Food Lovers </h1>';
+// var salary = 500 + 500;
+// // console.log(salary);
+
+// // alert('welcome ' - salary);
+
+// // what if the salary is less that 1000 
+// // thank you 
+// // what if the salary is more than 1000 
+// // amazing
+
+// //conditions
+// // if(salary < 1000 )
+// // {
+// //   console.log('thank you');
+// // }else if(salary > 1000)
+// // {
+// //   console.log('amazing');
+// // }else if(salary = 1000){
+// //   console.log('cool');
+// // }
+
+// // if(salary == 1000 )
+// // {
+// //   console.log('thank you');
+// // }
+// // else if(salary >= 1000)
+// // {
+// //   console.log('amazing');
+// // }
+// // else if(salary <= 1000){
+// //   console.log('cool');
+// // }
+
+// // 3 > 3 false 3 >= 3 true
+// // 3 < 3 false 3 <= 3 true
+
+
+// // logical operators
+// //&& and
+// //|| or
+// //! not
+
+// //f && t >> f
+// //t && f >> f
+// //f && f >> f
+// //t && t >> t
+
+
+// //t || t >> t
+// //t || f >> t
+// //f || t >> t 
+// //f || f >> f
+
+// //f && f >>> f
+
+// //t || f >>> t
+// // if(salary === 1000 || userName === 'shihab'){
+// //   // console.log('true');
+// //   alert('true');
+// // }else{
+// //   console.log('false');
+// // }
+
+// //1000 !== 1000
+// // if(salary !== 1000){
+// //   console.log('true');
+// // }else{
+// //   console.log('false');
+// // }
+// //ask the user to enter his/her age
+
+// // var userAge = prompt('please enter your age?');
+// // console.log('user age ' + userAge);
+
+// var x = prompt('please enter your name');
+// var x;
+// console.log(x);
+
+// //ask the user a type of food ( mansaf , shawarmah).
+// // if the user enter mansaf print a mansaf image
+
+// //if the user enter shawarmah print a shawarmah image
+
+var foodType = prompt('please enter a food type (mansaf , shawarmah)?');
+
+while (foodType !== 'mansaf' && foodType !== 'shawarmah') {
+  foodType = prompt('please enter a food type (mansaf , shawarmah)?');
+}
+console.log(foodType);
+// // console.log('foodType datatype ' 
+// // + typeof foodType);
+var mealsNumber = prompt('how many meals do you want to order?');
+
+for (var i = 1; i <= mealsNumber; i++) {
+
+  if (foodType == 'mansaf') {
+    var image = '<img src="https://www.experiencejordan.com/wp-content/uploads/2016/10/Blog-Headers2-1.jpg" alt= "mansaf"/>';
+    //create mansaf image
+    document.write(image);
+
+  } else if (foodType == 'shawarmah') {
+    //create shawarmah image
+    var image2 = '<img src="img/shawarmah.jpg" alt= "shawarmah"/>';
+    document.write(image2);
+  }
+}
+
+
+// var mealsNumber = prompt('please enter how many meals do you want to order?');
+
+// if(mealsNumber === 3){
+//   console.log('ok i can do that');
+// }else {
+//   console.log("sorry we can't do that");
+// }
+
+// var num = Number(prompt('please enter the number for the iterations ?'));
+
+// var startPoint = Number(prompt('please enter a start point'));
+
+//num = 100
+//startPoint = 1
+//t
+//i inside the loop 1
+//0
+//i inside the loop 0
+//-1
+//i inside the loop -1
+//-2
+
+// for(var i= startPoint;i<=num;i--){
+//   console.log('i inside the loop ',i);
+// }
+// // console.log('i outside the loop ',i)
